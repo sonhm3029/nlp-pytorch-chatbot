6331532bf4e0a3c19185ec986477929b7c4b8cfc
Son Hoang <hoangminhson29102001@gmail.com>
2022-09-17 15:44:37 +0700

    add chatbot GUI

--- src/chat.py
+++ src/chat.py
@@ -24,13 +24,10 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Simpson"
-print("Let's chat! type 'quit' to exit")
-while True:
-    sentence = input('You: ')
-    if sentence == 'quit':
-        break
-    sentence = tokenize(sentence)
+bot_name= "Simpson"
+
+def get_response(msg):
+    sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X =torch.from_numpy(X)
@@ -45,6 +42,8 @@ while True:
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent['tag']:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand...")
+                return (f"{random.choice(intent['responses'])}")
+    return (f"I do not understand...")
+
+
+    
